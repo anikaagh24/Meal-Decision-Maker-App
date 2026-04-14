18efd5010b4ed09ab7dab52f3e9358439686bac1
anikaagh24 <anikamakter721@gmail.com>
2026-04-14 09:36:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = document.querySelector(".breakfast-button")
+let button2 = document.querySelector(".lunch-button")
+let button3 = document.querySelector(".dinner-button")
+
+button.addEventListener("click", function() {
+});
+
+button2.addEventListener("click", function() {
+});
+
+button3.addEventListener("click", function() {
+});
+
